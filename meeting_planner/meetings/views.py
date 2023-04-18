86df9156abc0207c96646386555d5e4ec5dd7473
todor1 <todorowa_el@abv.bg>
2023-04-18 17:02:56 +0300

    Finished course Getting Started with Django

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
 from .models import Meeting, Room
-
+# from django.forms import modelform_factory
+### using custom class instead of modelform_factory
+from .forms import MeetingForm 
 
 def detail(request, id):
     # meeting = Meeting.objects.get(pk=id) ### throws an error if id not available
@@ -28,3 +29,27 @@ def detail(request, id):
 def rooms_list(request):
     return render(request, "meetings/rooms_list.html",
                   {"rooms": Room.objects.all()})
+    
+# Create a new form based on our Meeting class (imported from models)
+# exclude=[]-> see all the fields from my meeting model in the html form
+# after the function call below the result is a newly generated ModelForm class
+# MeetingForm is a Class, not a regular object
+# MeetingForm = modelform_factory(Meeting, exclude=[]) ### MeetingForm from custom class used instead
+
+def new(request):
+    if request.method == "POST":
+        # form hase been submitted, process data
+        form = MeetingForm(request.POST)
+        # check if all necessary fields are filled in 
+        # and whether they contain proper data types 
+        # use the check always before saving data to the database, both for security and consistency reasons
+        if form.is_valid():
+            form.save()                        
+            return redirect("welcome")
+    else:
+        form = MeetingForm() # instance of the MeetingForm class
+    # form below will be displayed in 2 cases:
+    # either the request method was not POST and we created and empty meeting 
+    # or the user has not filled it properly (is_valid=false, not passing the validity check), 
+    # the inaccurate data will be shown again, and the is_valid method will have added errors to the invalid fields
+    return render(request, "meetings/new.html", {'form': form})
